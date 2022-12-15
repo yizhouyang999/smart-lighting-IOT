189e6c3cc66aca518f71b639fa0719713277bc2d
Yizhou Yang <yangyizhou99@gmail.com>
2022-12-15 17:15:54 +0100

    Fea: toast for login page

--- android_app/app/src/main/java/com/example/iotlab2app/LoginActivity.java
+++ android_app/app/src/main/java/com/example/iotlab2app/LoginActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,18 +24,69 @@ public class LoginActivity extends AppCompatActivity {
         TextView password = (TextView) findViewById(R.id.password);
 
         MaterialButton login_button = (MaterialButton) findViewById(R.id.login_button);
+        MaterialButton help_button = (MaterialButton) findViewById(R.id.forget_password_button);
 
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
+                    showToastSuccessful();
                     openMainActivity();
                 } else {
-                    Toast.makeText(LoginActivity.this, "LOGIN Failed", Toast.LENGTH_SHORT).show();
+                    showToastFailed();
                 }
             }
         });
+
+        help_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToastHelp();
+            }
+        });
+    }
+
+    /**
+     * show toast when login successfully
+     */
+    public void showToastSuccessful(){
+        LayoutInflater inflater=getLayoutInflater();
+        View layout=inflater.inflate(R.layout.toast_successful,(ViewGroup) findViewById(R.id.toast_successful_root));
+        Toast toast=new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM,0,400);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+
+        toast.show();
+    }
+
+    /**
+     * show toast when login failed
+     */
+    public void showToastFailed(){
+        LayoutInflater inflater=getLayoutInflater();
+        View layout=inflater.inflate(R.layout.toast_failed,(ViewGroup) findViewById(R.id.toast_failed_root));
+        Toast toast=new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM,0,400);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+
+        toast.show();
+    }
+
+
+    /**
+     * show toast when user need help
+     */
+    public void showToastHelp(){
+        LayoutInflater inflater=getLayoutInflater();
+        View layout=inflater.inflate(R.layout.toast_phone,(ViewGroup) findViewById(R.id.toast_phone_root));
+        Toast toast=new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM,0,400);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+
+        toast.show();
     }
 
 
